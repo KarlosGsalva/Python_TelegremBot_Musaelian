f15f76d943cc879d1d1967fcf03c2f999b123ce0
Karlen M <karlenmusaelian@gmail.com>
2024-01-20 19:07:25 +0300

    steps counter swaped, make and fix check_winner_funcion, fixed texts, extract check_coord_funcion

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from itertools import cycle
 class TicTacToe:
     def __init__(self, player1='X', player2='O'):
         self.player1 = player1
-        self.player1_steps = 0
+        self.player1_steps = False
         self.player2 = player2
-        self.player2_steps = 0
+        self.player2_steps = False
         self.player_turn = cycle((self.player1, self.player2))
         self.board = [[''] * 3 for _ in range(3)]
         self.max_value_width = max(map(len, self.board))
@@ -20,44 +20,52 @@ class TicTacToe:
         else:
             print(self._non_first_step_warning(player))
 
-        self.draw_board()
+        self._draw_board()
         x, y = int(input('Координата х: ')), int(input('Координата y: '))
 
-        while not (0 <= x < 3 and 0 <= y < 3 and self.board[x][y] != 'X' and self.board[x][y] != 'O'):
+        while not self._check_right_coord(x, y, self.board):
             print(self._mistake_input_warning())
             x, y = int(input('Координата х: ')), int(input('Координата y: '))
-            continue
 
-        print(f"Отлично, теперь ход переходит к сопернику\n")
-        return self.make_move(player, x, y)
+        print(f"\nОтлично, теперь ход переходит к сопернику\n")
+        self._make_move(player, x, y)
 
-    def make_move(self, player: str, x: int, y: int):
+        if self._check_win(player, self.board):
+            self._draw_board()
+            print(self._winner_warning(player))
+            return
+        return self.ask_move()
+
+    def _make_move(self, player: str, x: int, y: int):
         self.board[x][y] = player
         match player:
             case self.player1:
-                self.player1_steps += 1
+                self.player1_steps = True
             case self.player2:
-                self.player2_steps += 1
-        self.ask_move()
+                self.player2_steps = True
 
     @staticmethod
-    def check_win(player: str, board: list[list]):
+    def _check_win(player: str, board: list[list]):
         for i in range(3):
+            # Проверка по горизонтали
             if board[i] == [player, player, player]:
                 return True
 
+            # Проверка по вертикали
             if board[0][i] == player and board[1][i] == player and board[2][i] == player:
                 return True
 
+        # Проверка по диагонали слева направо
         if board[0][0] == player and board[1][1] == player and board[2][2] == player:
             return True
 
+        # Проверка по диагонали справа налево
         if board[0][2] == player and board[1][1] == player and board[2][0] == player:
             return True
 
         return False
 
-    def draw_board(self) -> None:
+    def _draw_board(self) -> None:
         for x_coord in range(len(self.board)):
             for y_coord in range(len(self.board)):
                 if self.board[x_coord][y_coord]:
@@ -71,6 +79,12 @@ class TicTacToe:
                 print("-" * 16)
 
     @staticmethod
+    def _check_right_coord(x, y, board) -> bool:
+        if not (0 <= x < 3 and 0 <= y < 3 and board[x][y] != 'X' and board[x][y] != 'O'):
+            return False
+        return True
+
+    @staticmethod
     def _first_step_warning(player):
         text = (f"Привет, Игрок {player}, пожалуйста посмотрите на доску и выберите координаты на "
                 f"которые Вы хотели бы поставить {player},\nпри этом координаты должны быть в "
@@ -79,16 +93,22 @@ class TicTacToe:
 
     @staticmethod
     def _non_first_step_warning(player):
-        text = (f"\nЧто ж, Игрок {player}, давайте продолжим выбери координаты на "
+        text = (f"Что ж, Игрок {player}, давайте продолжим выбери координаты на "
                 f"которые ты хотел бы поставить {player},\nпри этом координаты должны "
-                f"быть в диапазоне от 0 до 2 включительно")
+                f"быть в диапазоне от 0 до 2 включительно, также учтите, что место, \n"
+                f"по указанным координатам на поле, не должно быть занято ;)\n")
         return text
 
     @staticmethod
     def _mistake_input_warning():
         text = (f"\nВы ввели некоректное число, напомним, координаты должны быть в "
-                f"диапазоне от 0 до 2 включительно также следует учесть, чтобы поле\n"
-                f"не было занято Вами или соперником ;)")
+                f"диапазоне от 0 до 2 включительно\n"
+                f"также следует учесть, чтобы поле не было занято Вами или соперником ;)")
+        return text
+
+    @staticmethod
+    def _winner_warning(player):
+        text = f"\nИтак у нас появился победитель, Игрок {player}, поздравляем ;) !"
         return text
 
     def change_player(self):
