1f47b998874ad6691120fc02c3a644f59f5a1f77
Karlen M <karlenmusaelian@gmail.com>
2024-01-24 00:02:25 +0300

    created base class NotesApp

---
+++ notesapp.py
@@ -0,0 +1,8 @@
+class NotesApp:
+    def __init__(self, note_text: str, note_name: str) -> None:
+        self.notes_list = []
+        self.note_text = note_text
+        self.note_name = note_name
+
+
+
