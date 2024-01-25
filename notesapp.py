ec7a4368397311c8a7d6f26407de56a84ccca79a
Karlen M <karlenmusaelian@gmail.com>
2024-01-25 23:23:14 +0300

    base of notes_app

--- notesapp.py
+++ notesapp.py
@@ -1,22 +1,21 @@
 from os import path
+from sys import stdin
 
 
 class NotesApp:
     def __init__(self) -> None:
-        self.note_text = input("Пожалуйста, введите текст заметки: ")
-        self.note_name = input("Пожалуйста, введите название заметки: ")
-        self.notes_list = []
+        self.note_name = input("Введите название заметки: ")
+        self.note_text = None
 
-    def build_note(self) -> None:
-        self.notes_list.append(self.note_name)
-        with open(f"{self.note_name}.txt", "w", encoding="utf-8") as notes_file:
-            notes_file.write(self.note_text)
+    def create_note(self) -> None:
+        with open(f"{self.note_name}_note.txt", "w", encoding="utf-8") as note_file:
+            line = stdin.readline()
+            for line in self.note_text:
+                notes_file.writelines(line)
         print(f"Заметка {self.note_name} создана")
 
     def read_notes(self) -> None:
-        print("Вам сейчас доступны следующие заметки:")
-        for note_name in self.notes_list:
-            print(note_name)
+        self.show_existing_notes()
         output_note = input("Введите название необходимой: ")
         if path.isfile(f"{output_note}.txt"):
             with open(f"{output_note}.txt", "r", encoding="utf-8") as note_file:
@@ -25,6 +24,14 @@ class NotesApp:
         else:
             print("Запрашиваемый файл отсутствует или введено некорректное название")
             self.read_notes()
+
+    @staticmethod
+    def show_existing_notes() -> None:
+        print("Вам сейчас доступны следующие заметки:")
+        folder_path = path.dirname(__file__)
+        existing_notes_list = os.listdir(folder_path)
+        for note in [file for file in existing_notes_list if file.endswith('note.txt')]:
+            print(note)
 
 
 make_note = NotesApp()
