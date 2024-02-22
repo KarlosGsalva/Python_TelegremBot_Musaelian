db44cbeeb792edf6a0987326e70cc786edd0fcf9
Karlen M <karlenmusaelian@gmail.com>
2024-02-22 20:25:20 +0300

    fixed keyboards

--- make_notes_bot/keyboards.py
+++ make_notes_bot/keyboards.py
@@ -19,22 +19,30 @@ async def _make_callback_inline_buttons(mode: str) -> list | None:
     try:
         buttons: list[InlineKeyboardButton] = []
         files = await asyncio.to_thread(lambda: async_notes_app.gather_all_notes())
+
         # Создаем кнопки в зависимости от mode'а
         if mode == 'show_sorted_notes':
             buttons = await async_notes_app.notes_in_inline_buttons()
             return buttons
+
         if mode == 'show_notes':
             buttons = await async_notes_app.notes_in_inline_buttons(False)
             return buttons
+
         for note in files:
             if mode == 'delete':
-                button = InlineKeyboardButton(text=f"Удалить {note[:-9]}", callback_data=f"delete_{note}")
+                button = InlineKeyboardButton(text=f"Удалить {note[:-9]}",
+                                              callback_data=f"delete_{note}")
             elif mode == 'read':
-                button = InlineKeyboardButton(text=f"Прочитать {note[:-9]}", callback_data=f"read_{note}")
+                button = InlineKeyboardButton(text=f"Прочитать {note[:-9]}",
+                                              callback_data=f"read_{note}")
             elif mode == 'edit':
-                button = InlineKeyboardButton(text=f"Редактировать {note[:-9]}", callback_data=f"edit_{note}")
+                button = InlineKeyboardButton(text=f"Редактировать {note[:-9]}",
+                                              callback_data=f"edit_{note}")
+
             if button is not None:
                 buttons.append(button)
+
         buttons.append(cancel_button)
         return buttons
     except Exception as e:
