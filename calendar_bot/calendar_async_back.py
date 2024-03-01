bf86dedeb750bf5256979272df3d03d4ba93978d
Karlen M <karlenmusaelian@gmail.com>
2024-03-01 11:44:01 +0300

    added try \ except to calendar_async_back part

--- calendar_bot/calendar_async_back.py
+++ calendar_bot/calendar_async_back.py
@@ -11,16 +11,19 @@ async def write_event_in_txt_file(event_name: str,
                                   event_date: date = None,
                                   event_time: str = None,
                                   event_details: str = None):
-    formatted_date = event_date if event_date else "Дата не указана"
-    formatted_time = event_time if event_time else "Время не указано"
-    formatted_details = event_details if event_details else "Описание отсутствует"
-    full_data = (f"Событие {event_name}\n"
-                 f"Дата события {formatted_date}\n"
-                 f"Время события {formatted_time}\n"
-                 f"Описание события {formatted_details}")
+    try:
+        formatted_date = event_date if event_date else "Дата не указана"
+        formatted_time = event_time if event_time else "Время не указано"
+        formatted_details = event_details if event_details else "Описание отсутствует"
+        full_data = (f"Событие {event_name}\n"
+                     f"Дата события {formatted_date}\n"
+                     f"Время события {formatted_time}\n"
+                     f"Описание события {formatted_details}")
 
-    async with aiof.open(f"{event_name}.txt", mode="w", encoding="utf-8") as event_file:
-        await event_file.write(full_data)
+        async with aiof.open(f"{event_name}.txt", mode="w", encoding="utf-8") as event_file:
+            await event_file.write(full_data)
+    except Exception as e:
+        print(f"Произошла ошибка{e}")
 
 
 # Функция записи события в json файл
@@ -28,60 +31,77 @@ async def write_event_in_json_file(event_name: str,
                                    event_date: date = None,
                                    event_time: str = None,
                                    event_details: str = None):
-    formatted_date = event_date if event_date else "Дата не указана"
-    formatted_time = event_time if event_time else "Время не указано"
-    formatted_details = event_details if event_details else "Описание отсутствует"
+    try:
+        formatted_date = event_date if event_date else "Дата не указана"
+        formatted_time = event_time if event_time else "Время не указано"
+        formatted_details = event_details if event_details else "Описание отсутствует"
 
-    dump_object = {event_name: {"Дата события": formatted_date,
-                                "Время события": formatted_time,
-                                "Описание события": formatted_details}}
+        dump_object = {event_name: {"Дата события": formatted_date,
+                                    "Время события": formatted_time,
+                                    "Описание события": formatted_details}}
 
-    async with aiof.open(f"{event_name}.json", mode="w", encoding="utf-8") as json_file:
-        await json_file.write(json.dumps(dump_object, ensure_ascii=False, indent=4))
+        async with aiof.open(f"{event_name}.json", mode="w", encoding="utf-8") as json_file:
+            await json_file.write(json.dumps(dump_object, ensure_ascii=False, indent=4))
+    except Exception as e:
+        print(f"Произошла ошибка{e}")
 
 
 # Чтение имеющихся событий в указанной или базовой директории
 def gather_having_events(path_for_search=None):
-    if path_for_search is None:
-        path_for_search = pathlib.Path(__file__).parent
-    else:
-        path_for_search = pathlib.Path(path_for_search)
+    try:
+        if path_for_search is None:
+            path_for_search = pathlib.Path(__file__).parent
+        else:
+            path_for_search = pathlib.Path(path_for_search)
 
-    events: list = [event.name for event in path_for_search.glob("*.json")]
-    return events
+        events: list = [event.name for event in path_for_search.glob("*.json")]
+        return events
+    except Exception as e:
+        print(f"Произошла ошибка{e}")
 
 
 # Читаем запрошенное событие из файла
 async def read_event(event_name: str) -> dict:
-    async with aiof.open(f"{event_name}", mode="r", encoding="utf-8") as json_file:
-        content = await json_file.read()
-        event_data = json.loads(content)
-    return event_data
+    try:
+        async with aiof.open(f"{event_name}", mode="r", encoding="utf-8") as json_file:
+            content = await json_file.read()
+            event_data = json.loads(content)
+        return event_data
+    except Exception as e:
+        print(f"Произошла ошибка{e}")
 
 
 # Форматируем словарь для вывода пользователю
 def format_event_data(event_data: dict) -> str:
-    event_name = ''.join(event_data.keys())
-    event_details = "\n".join(f"{name}: {text}" for name, text in event_data[event_name].items())
-    return event_name + "\n" + event_details
+    try:
+        event_name = ''.join(event_data.keys())
+        event_details = "\n".join(f"{name}: {text}" for name, text in event_data[event_name].items())
+        return event_name + "\n" + event_details
+    except Exception as e:
+        print(f"Произошла ошибка{e}")
 
 
 # Изменяем выбранный пункт события
 async def change_event_point(event_name: str, event_point: str, new_data: str) -> None:
-    async with aiof.open(f"{event_name}.json", mode="r", encoding="utf-8") as old_event:
-        old_data = await old_event.read()
-        deserialised_data = json.loads(old_data)
+    try:
+        async with aiof.open(f"{event_name}.json", mode="r", encoding="utf-8") as old_event:
+            old_data = await old_event.read()
+            deserialised_data = json.loads(old_data)
 
-    deserialised_data[event_name][event_point] = new_data
+        deserialised_data[event_name][event_point] = new_data
 
-    async with aiof.open(f"{event_name}.json", mode="w", encoding="utf-8") as new_event:
-        await new_event.write(json.dumps(deserialised_data))
+        async with aiof.open(f"{event_name}.json", mode="w", encoding="utf-8") as new_event:
+            await new_event.write(json.dumps(deserialised_data))
+    except Exception as e:
+        print(f"Произошла ошибка{e}")
 
 
 # Удаляем событие
 async def delete_event(event_name: str):
-    await aiof_os.remove(event_name)
-
+    try:
+        await aiof_os.remove(event_name)
+    except Exception as e:
+        print(f"Произошла ошибка{e}")
 
 
 
