a52b8245251741a5cb9bb958793598455d914e31
Karlen M <karlenmusaelian@gmail.com>
2024-05-28 23:10:56 +0300

    fixed admin.py

--- calendar_admin/bot_admin/admin.py
+++ calendar_admin/bot_admin/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import (User, Event, BotStatistics, Meeting, MeetingParticipant,
-                     MeetingParticipantInline, MeetingAdmin)
+from .models import (User, Event, BotStatistics, Meeting,
+                     MeetingParticipant, MeetingAdmin)
 
 
 admin.site.register(MeetingParticipant)
