99a46a335d80dbd2fd068b4f9d5e234dcbfbb532
Karlen M <karlenmusaelian@gmail.com>
2024-06-11 18:39:38 +0300

    added test task for api, json, csv

--- calendar_admin/tests/test_api.py
+++ calendar_admin/tests/test_api.py
@@ -1,7 +1,24 @@
 import requests
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+NGROK_URL = os.getenv("NGROK_URL")
+user_id = 6594296991
 
 
-# def test_external_api_integration():
-#     response = requests.get('https://api.example.com/data')
-#     assert response.status_code == 200
-#     assert 'data' in response.json()
+def test_accessibility_api():
+    response = requests.get(f"{NGROK_URL}api")
+    assert response.status_code == 200
+
+
+def test_accessibility_export_json():
+    response = requests.get(f"{NGROK_URL}export/json/?id={user_id}")
+    assert response.status_code == 200
+
+
+def test_accessibility_export_csv():
+    response = requests.get(f"{NGROK_URL}export/csv/?id={user_id}")
+    assert response.status_code == 200
